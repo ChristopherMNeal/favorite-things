d86acd411f5548baca7fc11ddaba51cb04d2825c
Christopher Neal <christopher.m.neal@gmail.com>
2021-10-25 10:19:25 -0700

    Add HTML, add UI javascript
    Co-authored-by: Jeff Lai <pjefflai@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("form#favorite-things").submit(function(event) {
+    event.preventDefault();
+    const favOne = $("input#fav-things-1").val();
+    const favTwo = $("input#fav-things-2").val();
+    const favThree = $("input#fav-things-3").val();
+    const favArray = [favOne, favTwo, favThree];
+    favArray.push("hello");
+    favArrayNew = favArray.concat(favArray);
+
+    const brandNewArray = [];
+    brandNewArray.push(favArrayNew[1], favArrayNew[0], favArrayNew[2]);
+    
+    $("#fav-things-list").append("<li>" + favArrayNew[1] + "</li> <li>" + favArrayNew[0] + "</li> <li>" + favArrayNew[2] + "</li>")
+  });  
+});
+
